97a897ac945f3972804012e5a3949437c42d3959
altaris33 <jeremie.laera@live.fr>
2026-05-21 19:54:29 +0200

    Switched to using an array of float values

--- arrays.c
+++ arrays.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /*
 
 Goal: Accounting program that Keeps track of money flow in a company
@@ -18,77 +17,37 @@ int main() {
 
     printf("Arrays: Working in a poor way\n");
 
-    // let's see a case where using an array could be useful
-    // let's imagine a solution without an array and see the limitations
-    // need for as many variables as need to store our inputs/outputs
-    // we will store a bunch of variables of the same type
     const int MAX_IO = 5;
-    int io_count = 0;
-    float io_1, io_2, io_3, io_4, io_5;
+    int io_count;
+    float io_array[MAX_IO];
 
-    while (io_count >= 0 && io_count < MAX_IO)
+    for (io_count = 0; io_count < MAX_IO; io_count++)
     {
         float value;
         printf("Insert an amount of money (0 to end):\n");
         printf("[%d/%d] ", io_count + 1, MAX_IO);
         scanf("%f", &value);
 
+        io_array[io_count] = value;
+
         if (value == 0)
         {
             break;
-        }
-        io_count++;
-
-        // assign the value entered by the user to the corresponding variables
-        // the amount of code gets messy/and difficult to maintain
-        // if we have to store a lot of values (100, 200...), the code will get huge
-        // so it is not a scalable solution
-        switch (io_count)
-        {
-        case 1:
-            io_1 = value;
-            break;
-        case 2:
-            io_2 = value;
-            break;
-        case 3:
-            io_3 = value;
-            break;
-        case 4:
-            io_4 = value;
-            break;
-        case 5:
-            io_5 = value;
-            break;
-        default:
-            printf("Unexpected value.");
-            return -1;
         }
     }
 
     printf("-------------------\n");
     printf("List of operations:\n");
     
-    // messy code also, hard to maintain
-    switch (io_count)
+    float sum = 0;
+    for (int i = 0; i < io_count; i++)
     {
-    case 5:
-        printf("5 | %9.2f$\n", io_5);
-    case 4:
-        printf("4 | %9.2f$\n", io_4);
-    case 3:
-        printf("3 | %9.2f$\n", io_3);
-    case 2:
-        printf("2 | %9.2f$\n", io_2);
-    case 1:
-        printf("1 | %9.2f$\n", io_1);
-        break;                
-    default:
-        printf("Nothing to print.\n");
-        break;
+        printf("%d | %9.2f$\n", (i+1), io_array[i]);
+        sum += io_array[i];
     }
 
-    // print the total
+    printf("-------------------\n");
+    printf("Total: %9.2f$\n", sum);
 
     return 0;
 }
